0c720e7d3c3ad2691807ad4e0d0851b6dcf637cf
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2021-09-26 00:38:00 -0400

    pixie dust

---
+++ docs/dex.js
@@ -0,0 +1,8 @@
+//l;
+NipdEx
+	End Point
+		'https://api.thegraph.com/subgraphs/name/nipple-labs/nipplelabs'
+	Full History
+		'{  uniswapDayDatas{    id    date    totalVolumeUSD    dailyVolumeUSD    dailyVolumeFTM    totalLiquidityUSD    totalLiquidityFTM    __typename    txCount    totalVolumeFTM    dailyVolumeUntracked}}';
+	Current Stat
+		'{  uniswapFactory(id:"0x41A75C3d6CDe54170e6a67d8D61752cc8Abb0275"){    totalVolumeUSD    txCount    id    pairCount    totalVolumeFTM    totalLiquidityUSD    totalLiquidityFTM    untrackedVolumeUSD    }}'
